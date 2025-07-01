1ae52d3e52f2a3a25710b6bb94b6e377ef940ae9
Leonardo Kowacic <86630142+Mouldor@users.noreply.github.com>
2025-07-01 17:43:53 -0300

    Update GerenciadorDePalavras.java

--- jogo/src/jogo/GerenciadorDePalavras.java
+++ jogo/src/jogo/GerenciadorDePalavras.java
@@ -11,10 +11,21 @@ public class GerenciadorDePalavras {
     private final String caminhoArquivo;
     private final Random random = new Random();
 
+    //Recebe o caminho do arquivo contendo as palavras e o armazena no atributo caminhoArquivo.
     public GerenciadorDePalavras(String caminhoArquivo) {
         this.caminhoArquivo = caminhoArquivo;
     }
 
+    //Lê todas as linhas do arquivo especificado pelo caminho.
+
+    //Cada linha é uma palavra.
+
+    //Se o arquivo estiver vazio, retorna null.
+
+    //Se houver conteúdo, sorteia aleatoriamente uma palavra da lista e retorna.
+
+    //Se ocorrer algum erro (por exemplo, o arquivo não existe), imprime a mensagem de erro e retorna null.
+    
     public String sortearPalavra() {
         try {
             List<String> palavras = Files.readAllLines(Paths.get(caminhoArquivo));
