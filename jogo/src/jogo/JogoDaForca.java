37a16aaf13117845066e4189cbfa578178a06261
Leonardo Kowacic <86630142+Mouldor@users.noreply.github.com>
2025-07-01 17:44:19 -0300

    Update JogoDaForca.java

--- jogo/src/jogo/JogoDaForca.java
+++ jogo/src/jogo/JogoDaForca.java
@@ -1,5 +1,8 @@
 package jogo;
 
+    //Instancia um novo objeto Partida — ou seja, cria uma nova sessão do jogo.
+
+	//Chama o método iniciar() dessa partida — é aí que o jogo realmente começa.
 public class JogoDaForca {
 
     public static void main(String[] args) {
